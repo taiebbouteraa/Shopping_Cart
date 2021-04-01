c85827d57214c5904568dbf3d7290b80775981a0
taiebbouteraa <80155553+taiebbouteraa@users.noreply.github.com>
2021-04-01 20:03:00 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,43 +24,51 @@ for(var i = 0; i < removeItemButtons.length; i++) {
 
 var plusValue = document.getElementsByClassName('plus-button')
 var minusValue = document.getElementsByClassName('minus-button')
-          
+        var quantityOfItems = document.getElementsByClassName('item-quantity')
 for(var i = 0; i < plusValue.length; i++){
     var button = plusValue[i]
     button.addEventListener('click', function(event){
         var buttonClicked = event.target
         var input = buttonClicked.parentElement.children[1]
-        var inputValue = input.value
+        var inputValue = input.innerText
         var newValue = parseInt(inputValue) + 1
-        input.value = newValue
+        input.innerHTML = newValue
+        
+    })
+    var quantityChange = quantityOfItems[i]
+    
+    quantityChange.addEventListener('click', function(action){
+        var newVal = action.target
+        
+        console.log(newVal)
     })
 }
 for(var i = 0; i < minusValue.length; i++){
     var button = minusValue[i]
-    var valval = document.getElementsByClassName('item-quantity')
+    
     button.addEventListener('click', function(event){
         var buttonClicked = event.target
         var input = buttonClicked.parentElement.children[1]
-        var inputValue = input.value;
+        var inputValue = input.innerHTML;
         var newValue = parseInt(inputValue) - 1
         
         
         if(newValue >= 1 ){
-            input.value = newValue
+            input.innerHTML = newValue
         }else{
-            input.value == 1
+            input.innerHTML == 1
         }
     
     })
-    
-    // valval.innerHTML = valvalyou 
 }
 
-// calculate sub price 
+// calculate sub price + total price
 var itemSinglePrices = document.getElementsByClassName('item-price')
 var quantityOfItems = document.getElementsByClassName('item-quantity')
 var itemSubTotals = document.getElementsByClassName('item-sub-total')
 var totalCart = document.getElementById('cart-total-price')
+var totalCart = document.getElementById('cart-total-price')
+var summed = 0
 
 for(var i = 0; i < quantityOfItems.length; i++){
     
@@ -68,18 +76,19 @@ for(var i = 0; i < quantityOfItems.length; i++){
     var valueOfItemSinglePrice = parseFloat(itemSinglePrice.innerText)
     
     var quantityOfItem = quantityOfItems[i]
-    var valueOfItemQuantity = parseInt(quantityOfItem.value)
+    var valueOfItemQuantity = parseInt(quantityOfItem.innerText)
     
     var newSubTotal = valueOfItemSinglePrice * valueOfItemQuantity
     var newSubTotalDecimal = newSubTotal.toFixed(2)
-    
+   
+    summed = summed + newSubTotal
+  
     var itemSubTotal = itemSubTotals[i]
     itemSubTotal.innerHTML = newSubTotalDecimal
     
-    // var tot = itemSinglePrices[i]
-    // var actualTotal = parseFloat(itemSubTotal.innerText)
-    // console.log(tot)
     
-    // totalCartPrice.innerHTML = 
+    var convToNum = parseInt(itemSubTotals[i].innerText)
+    
+    totalCart.innerHTML = summed.toFixed(2)
+    
 }
-//calculate total from sub totals
